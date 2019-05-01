c7365128104fc9c3aaec6b4481a850dea3426663
Thomas Vandivier <tvandivi@e1z2r6p13.42.us.org>
2019-04-30 17:28:57 -0700

    revert

---
+++ fillit_utils.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fillit_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvandivi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/23 22:23:45 by tvandivi          #+#    #+#             */
+/*   Updated: 2019/04/30 17:27:35 by tvandivi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+char	**error(void)
+{
+	char	**bad_board;
+
+	bad_board = ft_strsplit("invalid", ' ');
+	return (bad_board);
+}
+
+void	set_length(t_board *main_board)
+{
+	t_piece	*tmp;
+	t_piece	*nxt;
+	int		i;
+
+	i = -1;
+	tmp = main_board->tmp_board;
+	while (tmp)
+	{
+		nxt = tmp->next;
+		i++;
+		tmp = nxt;
+	}
+	main_board->tetra_count = i;
+}
+
+t_piece	*add_lst_piece(t_piece *tmp, char *buf, int i, int a)
+{
+	t_piece	*nxt;
+
+	tmp->next = (t_piece *)malloc(sizeof(t_piece) * 1);
+	nxt = tmp->next;
+	tmp->piece = &*ft_strsplit(buf, '\n');
+	tmp->p_num = i++;
+	tmp = nxt;
+	ft_bzero(buf, a);
+	return (tmp);
+}
+
+void	print_board(t_board *main_board)
+{
+	int	i;
+
+	i = 0;
+	while (i < main_board->tetra_count)
+	{
+		ft_putstr(main_board->solved_board[i++]);
+	}
+}
+
+void	print_pieces(t_board *mst)
+{
+	int		i;
+	t_piece	*tmp;
+	t_piece	*nxt;
+
+	i = 0;
+	tmp = mst->tmp_board;
+	while (tmp)
+	{
+		if (!(tmp->piece))
+			break ;
+		nxt = tmp->next;
+		while (i < 4)
+		{
+			ft_putstr(tmp->piece[i++]);
+			ft_putchar('\n');
+		}
+		ft_putnbr(tmp->p_num);
+		ft_putchar('\n');
+		i = 0;
+		tmp = nxt;
+	}
+}
---
+++ solve.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   solve.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvandivi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/04/24 11:19:41 by tvandivi          #+#    #+#             */
+/*   Updated: 2019/04/30 17:28:09 by tvandivi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fillit.h"
+
+void	normalize_blocks(t_board *main_board)
+{
+
+}
+
+void	generate_solution_board(t_board *main_board)
+{
+
+}
+
+void	solve(t_board *main_board)
+{
+	
+}
+
