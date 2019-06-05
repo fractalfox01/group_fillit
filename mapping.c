30d6096a963c4ffd0a1ec36bbb9408e697f6a9d8
Thomas Vandivier <tvandivi@e1z2r8p31.42.us.org>
2019-06-04 18:57:02 -0700

    mapping.c is norm'd

--- mapping.c
+++ mapping.c
@@ -3,52 +3,67 @@
 /*                                                        :::      ::::::::   */
 /*   mapping.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ezhukova <ezhukova@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tvandivi <tvandivi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 16:56:39 by ezhukova          #+#    #+#             */
-/*   Updated: 2019/06/04 17:38:28 by ezhukova         ###   ########.fr       */
+/*   Updated: 2019/06/04 18:55:12 by tvandivi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-void		map_pc(t_board *mst, t_piece *tetra, int col, int row)
+void		map_pc(t_board *mst, t_piece *t, int c, int r)
 {
-	if (mst && tetra)
+	if (mst && t)
 	{
-		mst->slv_b[row + (tetra->sym_arr[1])][col + (tetra->sym_arr[0])] = tetra->p_num + 'A';
-		mst->slv_b[row + (tetra->sym_arr[3])][col + (tetra->sym_arr[2])] = tetra->p_num + 'A';
-		mst->slv_b[row + (tetra->sym_arr[5])][col + (tetra->sym_arr[4])] = tetra->p_num + 'A';
-		mst->slv_b[row + (tetra->sym_arr[7])][col + (tetra->sym_arr[6])] = tetra->p_num + 'A';
+		mst->slv_b[r + (t->sym_arr[1])][c + (t->sym_arr[0])] = t->p_num + 'A';
+		mst->slv_b[r + (t->sym_arr[3])][c + (t->sym_arr[2])] = t->p_num + 'A';
+		mst->slv_b[r + (t->sym_arr[5])][c + (t->sym_arr[4])] = t->p_num + 'A';
+		mst->slv_b[r + (t->sym_arr[7])][c + (t->sym_arr[6])] = t->p_num + 'A';
 		mst->valid += 1;
 	}
 }
 
-int			check_area(t_board *mst, char **solution_board)
+int			c_h(char **s_b, t_piece *t, int c, int r)
 {
-	int	len;
-	t_piece *tetra = get_piece(mst, mst->cur);
-	if (!(tetra))
-		return (0);
-	int	col = tetra->col;
-	int	row = tetra->row;
-	int good = 0;
+	if (s_b[r + (t->sym_arr[1])][c + (t->sym_arr[0])] == '.' && \
+		s_b[r + (t->sym_arr[3])][c + (t->sym_arr[2])] == '.' && \
+		s_b[r + (t->sym_arr[5])][c + (t->sym_arr[4])] == '.' && \
+		s_b[r + (t->sym_arr[7])][c + (t->sym_arr[6])] == '.')
+		return (1);
+	return (0);
+}
 
-	len = mst->b_size;
-	if (mst && solution_board)
+int			area_helper(int l, t_piece *t, int c, int r)
+{
+	if ((((r + t->sym_arr[1])) > (l - 1) || ((c + t->sym_arr[0])) > l) || \
+		(((r + t->sym_arr[3])) > (l - 1) || ((c + t->sym_arr[2])) > l) || \
+		(((r + t->sym_arr[5])) > (l - 1) || ((c + t->sym_arr[4])) > l) || \
+		(((r + t->sym_arr[7])) > (l - 1) || ((c + t->sym_arr[6])) > l))
+		return (5);
+	return (0);
+}
+
+int			check_area(t_board *mst, char **s_b)
+{
+	int		l;
+	t_piece	*t;
+	int		c;
+	int		r;
+
+	t = get_piece(mst, mst->cur);
+	if (!(t))
+		return (0);
+	r = t->row;
+	c = t->col;
+	l = mst->b_size;
+	if (mst && s_b)
 	{
-		if ((((row + tetra->sym_arr[1])) > (len - 1) || ((col + tetra->sym_arr[0])) > len) || \
-		(((row + tetra->sym_arr[3])) > (len - 1) || ((col + tetra->sym_arr[2])) > len) || \
-		(((row + tetra->sym_arr[5])) > (len - 1) || ((col + tetra->sym_arr[4])) > len) || \
-		(((row + tetra->sym_arr[7])) > (len - 1) || ((col + tetra->sym_arr[6])) > len))
+		if (area_helper(l, t, c, r))
 			return (5);
-		if (solution_board[row + (tetra->sym_arr[1])][col + (tetra->sym_arr[0])] == '.' && \
-		solution_board[row + (tetra->sym_arr[3])][col + (tetra->sym_arr[2])] == '.' && \
-		solution_board[row + (tetra->sym_arr[5])][col + (tetra->sym_arr[4])] == '.' && \
-		solution_board[row + (tetra->sym_arr[7])][col + (tetra->sym_arr[6])] == '.')
+		if (c_h(s_b, t, c, r))
 		{
-			good = 4;
-			map_pc(mst, tetra, tetra->col, tetra->row);
+			map_pc(mst, t, t->col, t->row);
 			return (4);
 		}
 		else
@@ -57,7 +72,7 @@ int			check_area(t_board *mst, char **solution_board)
 	return (5);
 }
 
-int		start_mapping(t_board *mst)
+int			start_mapping(t_board *mst)
 {
 	int	b;
 
